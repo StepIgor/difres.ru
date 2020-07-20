e422fe599e9039e0c293db0f72c29d398e7c2c40
StepIgor <igor.stepanov@difres.ru>
2020-07-20 12:15:54 +0400

    update db with qna

--- js/db.js
+++ js/db.js
@@ -7,5 +7,9 @@ search_db = [
   ['Разминка.Счёт','count.difres.ru',['count.difres.ru','count','счёт','счет','игра']],
   ['Дневник','diary.difres.ru',['diary','diary.difres.ru','дневник']],
   ['Дневник | JS Store','jsstore.difres.ru/product?id=1',['дневник','diary','js','store','jsstore','jsstore.difres.ru']],
-  ['Разминка.Счёт | JS Store','jsstore.difres.ru/product?id=2',['счёт','count','js','store','jsstore','jsstore.difres.ru','счет']]
+  ['Разминка.Счёт | JS Store','jsstore.difres.ru/product?id=2',['счёт','count','js','store','jsstore','jsstore.difres.ru','счет']],
+  ['Вопросы и ответы | QnA DIFRES.RU', 'qna.difres.ru', ['qna', 'questions', 'answers', 'question', 'answer', 'вопросы', 'ответы', 'вопрос', 'ответ', 'qna.difres.ru']],
+  ['Какие команды нужно прописать в консоли для включения bhop на своем сервере CS:GO? | QnA DIFRES.RU', 'qna.difres.ru/question.html?id=1', ['cs', 'hop', 'хоп', 'кс', 'вопрос', 'команды', 'консоли']],
+  ['Как открыть диспетчер задач в Windows 10? | QnA DIFRES.RU', 'qna.difres.ru/question.html?id=2', ['диспетчер', 'задач', 'windows', '10', 'открыть']],
+  ['Как проще запомнить все цвета радуги? | QnA DIFRES.RU', 'qna.difres.ru/question.html?id=3', ['цвета', 'радуг', 'запомнить', 'выучить', 'вопрос']]
 ];
