9caf6757f3e6be0adb0d217dc60f0fa9761ae302
StepIgor <stepanow.igor@gmail.com>
2021-04-26 21:55:18 +0300

    Rebuilding! Now difres.ru is using Svelte!

---
+++ src/libs.js
@@ -0,0 +1,25 @@
+export function htmlspecialchars(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+
+    return text.replace(/[&<>"']/g, function (m) {
+        return map[m];
+    });
+}
+
+export function haveCommonElements(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item))
+}
+
+export function read_get() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
